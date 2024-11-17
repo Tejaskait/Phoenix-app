075ccb380f488c94b611f335a733436f3b8cc9fd
Tejas <tejaskait@gmail.com>
2024-11-17 13:29:34 +0530

    made basic client side app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Resources from './pages/Resources';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import OurServices from './pages/OurServices';
+import Careers from './pages/Careers';
+import Profile from './pages/Profile';
+import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  return <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/aboutus" element={<AboutUs/>} />
+        <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/ourservices" element={<OurServices />} />
+        <Route path="/carrers" element={<Careers />} />
+      </Routes>
+    </BrowserRouter>;
 }
