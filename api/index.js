c189f447a9d88ff962512d1f0fb3dd0e9a6963fe
Tejas <tejaskait@gmail.com>
2024-11-19 18:46:30 +0530

    create signup api

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/userroute.js'
+import userRouter from './routes/userroute.js';
+import authRouter from './routes/authroute.js';
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Connected to database');
 }).catch(err => {
@@ -12,6 +14,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 })
 
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
 
 app.listen(3000, ()=> {
     console.log('listening on 3000!!');
--- api/models/usermodel.js
+++ api/models/usermodel.js
@@ -4,51 +4,18 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        unique: true,
+        unique: true
     },
     userpassword: {
         type: String,
-        required: true,
+        required: true
     },
     useremail: {
         type: String,
         required: true,
-        unique: true,
-    },
-    userrole: {
-        type: String,
-        enum: ['admin', 'user'],
-        default: 'user',
-    },
-    userisActive: {
-        type: Boolean,
-        default: true,
-    },
-    usercreatedAt: {
-        type: Date,
-        default: Date.now,
-    },
-    userupdatedAt: {
-        type: Date,
-        default: Date.now,
-    },
-    userbranch: {
-        type: String,
-        required: true,
-    },
-    userdepartment: {
-        type: String,
-        required: true,
-    },
-    userbirthdate: {
-        type: String,
-        required: true,
-    },
-    usercontact: {
-        type: Number,
-        required: true,
+        unique: true
     }
-},
+}, 
     {
         timestamps: true,
     }
