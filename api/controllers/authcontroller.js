1d25cde179c1d11903845e4eff4a961fd5403734
Tejas <tejaskait@gmail.com>
2024-11-20 20:55:20 +0530

    created signin api

--- api/controllers/authcontroller.js
+++ api/controllers/authcontroller.js
@@ -1,5 +1,8 @@
 import User from "../models/usermodel.js";
 import bcryptjs from 'bcryptjs';
+import errorHandler from "../utils/error.js";
+import jwt from 'jsonwebtoken';
+
 export const signup= async (req,res,next) => {
     const { username, userpassword ,useremail} = req.body;
     const hashedPassword = bcryptjs.hashSync(userpassword, 10);
@@ -12,3 +15,22 @@ export const signup= async (req,res,next) => {
        next(error);
     }
 }; 
+
+export const signin = async (req,res,next) => {
+    const{ useremail, userpassword}=req.body;
+    try {
+        const validUser = await User.findOne({useremail});
+        if (!validUser) 
+            return next(errorHandler(404,'user not found'));
+        const validUserPassword = bcryptjs.compareSync(userpassword,validUser.userpassword);    
+        if (!validUserPassword) return next(errorHandler(401,'wrong credentials'));
+        const token = jwt.sign({id: validUser._id},process.env.JWT_SECRET);
+        const {userpassword: pass, ...rest}=validUser._doc;
+        res
+        .cookie('access_token',token,{httpOnly: true})
+        .status(200)
+        .json(rest);
+    } catch (error) {
+        next(error);
+    }
+};
